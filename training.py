fd605abb05d6b91e452c455b88f40bbd8188bf9f
simo4-4 <59541717+simo4-4@users.noreply.github.com>
2021-02-03 17:02:56 -0500

    Update training.py

--- training.py
+++ training.py
@@ -79,6 +79,5 @@ model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 model.save('chatbotmodel.h5', hist)
 
-#model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-#model.save('chatbot_model.model')
+
 print("Done")
